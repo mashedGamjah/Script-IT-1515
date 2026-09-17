272a8d7a1a4da6c97fb8eead042dc87b1cfeb55f
hyuni <34014733+mashedGamjah@users.noreply.github.com>
2026-09-17 15:51:05 -0700

    update: in class assignment

---
+++ week2/inClass.py
@@ -0,0 +1,19 @@
+x = 123
+y = "Hello, World!"
+z = 12.3
+
+x = 321
+y = "this is fun"
+z = 32.1
+
+print("i didn't want to leave this area blank")
+print(x)
+print(type(y))
+
+course = "CIT"
+num = "1515"
+
+print("A" + course + num)
+
+assignment_number = 2
+print(f'Assignment {assignment_number} for A{course} {num} is due today.')
