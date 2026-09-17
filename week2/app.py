744ed615d60f16e5c7577660a1c928a067b554f7
hyuni <34014733+mashedGamjah@users.noreply.github.com>
2026-09-17 16:08:07 -0700

    unrelated to class (just copying in class)

---
+++ week2/app.py
@@ -0,0 +1,8 @@
+school = "bcit"
+statement = "is a cool school"
+
+school_statement = school + " " + statement
+print(school_statement)
+
+another_statement = f"{school} realllly {statement}"
+print(another_statement)
